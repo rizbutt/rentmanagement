f8af1531e00e9ef63f98581f21bbbde0a7730657
Nouman-blip <attaussmalikki@gmail.com>
2024-08-07 17:18:45 +0500

    moved to models _types folder for more clearity in structure

--- types/user_type.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser extends Document {
-    [x: string]: any;
-    email: string;
-    password: string;
-    resetPasswordToken?: string;
-    resetPasswordExpires?: Date;
-    createPasswordResetToken?: () => string;
-  }
