e3ae57878c88a9b1b216f2d81942e8d1e3b23538
Nouman-blip <attaussmalikki@gmail.com>
2024-08-08 20:10:01 +0500

    tenant service and repository added a get function of tenants .

--- repositories/tenant_repository.ts
+++ repositories/tenant_repository.ts
@@ -1,6 +1,6 @@
 
 import Tenant from '../models/tenant_model';
-import { ITenant } from '@/types/tenant_type';
+import { ITenant } from '@/types/models_types/tenant_type';
 
 class TenantRepository {
   async create(tenant_data: ITenant): Promise<ITenant> {
@@ -9,6 +9,10 @@ class TenantRepository {
     return newTenant;
   }
 
+ // fetching all tenants data  rent  to tenant  service
+  async fetchAllTenants(user_id: string): Promise<ITenant[]> {
+    return await Tenant.find({ user_id: user_id }).populate('user_id').exec();
+  }
   
   
 }
