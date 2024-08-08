15aaaeb945c53971d7e45573345d6d397d1cc4f6
Nouman-blip <attaussmalikki@gmail.com>
2024-08-08 20:04:27 +0500

    property model service and repository class , api of post and get method for property in other part to use it

--- repositories/property_repository.ts
+++ repositories/property_repository.ts
@@ -1,5 +1,5 @@
 import Property from '../models/property_model';
-import { IProperty } from '@/types/property_type';
+import { IProperty } from '@/types/models_types/property_type';
 
 export class PropertyRepository {
   async create(propertyData: IProperty): Promise<IProperty> {
@@ -8,5 +8,21 @@ export class PropertyRepository {
     return await property.save();
   }
 
- 
+
+  //getting all properties data of that user 
+  async getAllPropertiesOfSpecificUser(user_id: string): Promise<IProperty[]> {
+    return await Property.find({ user_id: user_id }).populate('user_id').exec();
+  }
+  
+  /*
+   Fetch 
+
+  */
+
+
+
+
+   
+
 }
+
---
+++ types/models_types/property_type.ts
@@ -0,0 +1,62 @@
+import mongoose, { Document } from "mongoose";
+
+
+// Main Property Interface
+export interface IProperty extends Document {
+  propertyNo: string;
+  ownerName: string;
+  property_type: 'Villa' | 'Building' | 'Commercial';
+  address: string;
+  contractDuration: {
+    from: Date;
+    to: Date;
+  };
+  totalRent: number;
+  rentInstalments: [{
+    dueDate: Date;
+    amount: number;
+}];
+  billsToPay?: boolean;
+  paymentMethod?: 'Cash' | 'Cheque';
+  billsAccounts?: [
+    {
+      type?: string; // e.g., water/gas/electric
+      accountNo?: string;
+    }
+  ];
+  ownership_type: 'Owned' | 'Rented';
+  user_document?: {
+    data: Buffer; // Binary data for the file
+    contentType: string; // MIME type of the file
+  };
+  building_images?: [{
+    data: Buffer; // Path to the image file
+    description?: string; // Optional description or metadata
+  }]; // Array of building images
+  rentDetails?: {
+    rentAgreement: string; // Path to uploaded document
+    rentAmount: number;
+    rentTenure: number;
+    rentStartDate: Date;
+  };
+  buildingDetails: {
+    name:string,
+    address:string,
+    rooms: number;
+    kitchens: number;
+    lobbies: number;
+    bathrooms: number;
+    additionalFeatures?: string;
+  };
+  sections?: [{
+    sectionName?: string; // Dynamically named by the user (e.g., "section1", "section2")
+    sectionType?: 'Shared' | 'Single';
+    rooms?: number;
+    lobbies?:number;
+    kitchens?: number;
+    bathrooms?: number;
+  }]; // Array of dynamically named sections
+  user_id: mongoose.Schema.Types.ObjectId; // Reference to the user model
+  createdAt?: Date;
+  updatedAt?: Date;
+}
